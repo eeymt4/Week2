109be83c69093e94790ba5fa3e25d1185c677ceb
Martin_eeymt4 <56674268+eeymt4@users.noreply.github.com>
2019-10-17 22:31:09 +0100

    Worksheet 3 Initial commit. Hello.cpp written + compiled -> executable made, calc.cpp written not compiled

---
+++ WorkSheet3/calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+
+int main( int argc, char const *argv[] ) {
+  int a, b, c;
+  if( argc != 3 ) return 1;
+
+  std::string sa( argv[1] );
+  std::string sb( argv[2] );
+
+std::stringstream ssa ( sa );
+std::stringstream ssb ( sb );
+
+ssa>>a;
+ssb>>b;
+
+c = add( a, b );
+std::cout <<c;
+
+  return 0;
+}
+// calc.cpp
---
+++ WorkSheet3/hello.cpp
@@ -0,0 +1,7 @@
+//hello.cpp
+
+#include <iostream>
+ int main(int argc, char const *argv[]) {
+  std::cout << "Hello, Planet Earth!" << std::endl;
+  return 0;
+}
