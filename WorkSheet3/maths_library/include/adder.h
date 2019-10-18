613ba81d59bc89b7fec0c9366499fe7e20989ed3
Martin_eeymt4 <56674268+eeymt4@users.noreply.github.com>
2019-10-18 09:55:39 +0100

    Dynamis maths library created and compiled

--- WorkSheet3/maths_library/include/adder.h
+++ WorkSheet3/maths_library/include/adder.h
@@ -8,8 +8,35 @@
 // arbitrary but must be unique within the project
 #ifndef MATHSLIB_ADDER_H
 #define MATHSLIB_ADDER_H
-//prototype for our function
-int add( int a, int b);
+
+//we need to prefix our function names with an additional keyword which
+//is different depending on the operating system we are using, and whether
+//we are using or creating the library.
+//The variables "maths_EXPORTS" must be defined at build time if
+//we are building the library, but not if we are using it.
+#if defined(WIN32)|defined(_WIN32)
+    #ifdef maths_STATIC
+      //dont add any keywords if building a static library
+        #define MATHSLIB_API
+    #else
+        #ifdef maths_EXPORTS
+            #define MATHSLIB_API __declspec( dllexport )
+        #else
+            #define MATHSLIB_API __declspec( dllimport )
+        #endif
+    #endif
+#else
+      //MATHSLIB_API is defined as nothing if we are not on windows
+      #define MATHSLIB_API
+#endif
+
+//the above will include one of:
+// __declspec( dllexport)
+// __declspec( dllimport)
+//before declerations. This is a Microsoft specific extension to c/c++
+
+//prototype for our function including additional keyword (MATHSLIB_API)
+MATHSLIB_API int add( int a, int b);
 
 #endif
 
